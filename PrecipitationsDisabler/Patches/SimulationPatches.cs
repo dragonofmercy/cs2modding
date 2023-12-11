3b4379ff8795a41ccd0e9755ed93c8f4382fda13
Dragon <dragonofmercy@gmail.com>
2023-12-11 12:12:29 +0100

    Remove jetbrain annotations

--- PrecipitationsDisabler/Patches/SimulationPatches.cs
+++ PrecipitationsDisabler/Patches/SimulationPatches.cs
@@ -1,13 +1,11 @@
 ﻿using Game.Simulation;
 using HarmonyLib;
-using JetBrains.Annotations;
 
 namespace PrecipitationsDisabler.Patches;
 
 [HarmonyPatch(typeof(ClimateSystem), "UpdateSeason")]
 internal class ClimateSystemSampleClimatePatch
 {
-    [UsedImplicitly]
     private static void Postfix(ClimateSystem __instance)
     {
         __instance.temperature.value = 23.0f;
